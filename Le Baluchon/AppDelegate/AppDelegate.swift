c8700f3bc40d60cd4f3df48be36920331b9687e6
mabayon <mabayon@student.42.fr>
2020-06-05 09:07:21 +0200

    Files classifications

---
+++ Le Baluchon/AppDelegate/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Le Baluchon
+//
+//  Created by Mahieu Bayon on 05/06/2020.
+//  Copyright © 2020 Mabayon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
