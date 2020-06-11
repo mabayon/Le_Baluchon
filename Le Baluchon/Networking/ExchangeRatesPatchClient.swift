366cadc07873ced977a2ca1e549787811f90c31c
mabayon <mabayon@student.42.fr>
2020-06-11 17:13:22 +0200

    Creation and test of ExchangeRatesPatchClient

---
+++ Le Baluchon/Networking/ExchangeRatesPatchClient.swift
@@ -0,0 +1,58 @@
+//
+//  CurrencyPatchClient.swift
+//  Le Baluchon
+//
+//  Created by Mahieu Bayon on 08/06/2020.
+//  Copyright © 2020 Mabayon. All rights reserved.
+//
+
+import Foundation
+
+class ExchangeRatesPatchClient {
+    
+    let baseURL: URL
+    let session: URLSession
+    let responseQueue: DispatchQueue?
+    
+    init(baseURL: URL, session: URLSession, responseQueue: DispatchQueue?) {
+        self.baseURL = baseURL
+        self.session = session
+        self.responseQueue = responseQueue
+    }
+    
+    func getRates(completion: @escaping (ExchangeRates?, Error?) -> Void) -> URLSessionDataTask {
+        let url = URL(string: "rates", relativeTo: baseURL)!
+        let task = session.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+            
+            guard let response = response as? HTTPURLResponse,
+                response.statusCode == 200,
+                error == nil,
+                let data = data else {
+                    self.dispatchResult(error: error, completion: completion)
+                    return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let rates = try decoder.decode(ExchangeRates.self, from: data)
+                self.dispatchResult(models: rates, completion: completion)
+            } catch {
+                self.dispatchResult(error: error, completion: completion)
+            }
+        }
+        task.resume()
+        return task
+    }
+    
+    private func dispatchResult<Type>(models: Type? = nil,
+                                      error: Error? = nil,
+                                      completion: @escaping (Type?, Error?) -> Void) {
+        guard let responseQueue = self.responseQueue else {
+            completion(models, error)
+            return
+        }
+        responseQueue.async {
+            completion(models, error)
+        }
+    }
+}
--- Le BaluchonTests/Cases/Networking/ExchangeRatesPatchClientTests.swift
+++ Le BaluchonTests/Cases/Networking/ExchangeRatesPatchClientTests.swift
@@ -11,11 +11,254 @@ import XCTest
 
 class ExchangeRatesPatchClientTests: XCTestCase {
 
+    // MARK: - Instance Properties
+    var baseURL: URL!
+    var mockSession: MockURLSession!
     var sut: ExchangeRatesPatchClient!
+    var getRatesURL: URL {
+        return URL(string: "rates", relativeTo: baseURL)!
+    }
     
+    // MARK: - Test Lifecycle
     override func setUp() {
+        super.setUp()
+        baseURL = URL(string: "https://example.com/api/v1/")!
+        mockSession = MockURLSession()
+        sut = ExchangeRatesPatchClient(baseURL: baseURL, session: mockSession, responseQueue: nil)
     }
 
     override func tearDown() {
+        baseURL = nil
+        mockSession = nil
+        sut = nil
+        super.tearDown()
+    }
+    
+    // MARK: - Helper methods
+    
+    func whenGetRates(data: Data? = nil,
+                      statusCode: Int = 200,
+                      error: Error? = nil)
+        -> (calledCompletion: Bool, rates: ExchangeRates?, error: Error?) {
+
+            let response = HTTPURLResponse(url: getRatesURL,
+                                           statusCode: statusCode,
+                                           httpVersion: nil,
+                                           headerFields: nil)
+                        
+            var calledCompletion = false
+            var receivedRates: ExchangeRates? = nil
+            var receivedError: Error? = nil
+            
+            let mockTask = sut.getRates { (rates, error) in
+                calledCompletion = true
+                receivedRates = rates
+                receivedError = error as NSError?
+            } as! MockURLSessionDataTask
+
+            mockTask.completionHandler(data, response, error)
+            return (calledCompletion, receivedRates, receivedError)
+
+    }
+    
+    func verifyGetRatesDispatchedToMain(data: Data? = nil,
+                                        statusCode: Int = 200,
+                                        error: Error? = nil,
+                                        line: UInt = #line) {
+        // Given
+        mockSession.givenDispatchQueue()
+        sut = ExchangeRatesPatchClient(baseURL: baseURL,
+                                       session: mockSession,
+                                       responseQueue: .main)
+        
+        let expectation = self.expectation(description: "Completion wasn't called")
+        
+        // When
+        var thread: Thread!
+        let mockTask = sut.getRates { (rates, error) in
+            thread = Thread.current
+            expectation.fulfill()
+        } as! MockURLSessionDataTask
+        
+        let response = HTTPURLResponse(url: getRatesURL,
+                                       statusCode: statusCode,
+                                       httpVersion: nil,
+                                       headerFields: nil)
+        mockTask.completionHandler(data, response, error)
+        
+        // Then
+        waitForExpectations(timeout: 0.2) { _ in
+            XCTAssertTrue(thread.isMainThread, line: line)
+        }
+
+    }
+    // MARK: - Init - Tests
+    func test_init_sets_BaseURL() {
+        XCTAssertEqual(sut.baseURL, baseURL)
+    }
+    
+    func test_init_sets_Session() {
+        XCTAssertEqual(sut.session, mockSession)
+    }
+    
+    func test_init_sets_reponseQueue() {
+        // Given
+        let response = DispatchQueue.main
+        
+        // When
+        sut = ExchangeRatesPatchClient(baseURL: baseURL,
+                                       session: mockSession,
+                                       responseQueue: response)
+        XCTAssertEqual(sut.responseQueue, response)
+    }
+    
+    // MARK: - GetRates - Tests
+    func test_getRates_callsExpectedURL() {
+        // When
+        let mockTask = sut.getRates { (_, _) in
+            
+        } as! MockURLSessionDataTask
+        
+        // Then
+        XCTAssertEqual(mockTask.url, getRatesURL)
+    }
+    
+    func test_getRates_callsResumOnTask() {
+        // When
+        let mockTask = sut.getRates { (_, _) in } as! MockURLSessionDataTask
+        
+        // Then
+        XCTAssertTrue(mockTask.calledResume)
+    }
+    
+    func test_getRates_givenResponseStatusCode500_callsCompletion() {
+        // When
+        let result = whenGetRates(statusCode: 500)
+        
+        // Then
+        XCTAssertTrue(result.calledCompletion)
+        XCTAssertNil(result.rates)
+        XCTAssertNil(result.error)
+    }
+    
+    func test_getRates_givenError_callsCompletionWithError() throws {
+        let expectedError = NSError(domain: "com.RatesPatchTests", code: 42)
+        
+        // When
+        let result = whenGetRates(error: expectedError)
+        
+        // Then
+        XCTAssertTrue(result.calledCompletion)
+        XCTAssertNil(result.rates)
+
+        let actualError = try XCTUnwrap(result.error as NSError?)
+        XCTAssertEqual(actualError, expectedError)
+    }
+    
+    func test_getRates_givenValidJSON_callsCompletionWithRates() throws {
+        // Given
+        let data = try Data.fromJSON(fileName: "ExchangeRates")
+        
+        let decodoer = JSONDecoder()
+        let rates = try decodoer.decode(ExchangeRates.self, from: data)
+        
+        // When
+        let result = whenGetRates(data: data)
+        
+        // Then
+        XCTAssertTrue(result.calledCompletion)
+        XCTAssertNil(result.error)
+        XCTAssertEqual(result.rates, rates)
+    }
+    
+    func test_getRates_givenInvalidJSON_callsCompletionWithError() throws {
+        // Given
+        let data = try Data.fromJSON(fileName: "GET_ExchangeRates_MissingValuesResponse")
+        
+        var expectedError: NSError!
+        let decoder = JSONDecoder()
+      
+        do {
+            _ = try decoder.decode(ExchangeRates.self, from: data)
+        } catch {
+            expectedError = error as NSError
+        }
+        
+        // When
+        let result = whenGetRates(data: data)
+        
+        // Then
+        XCTAssertTrue(result.calledCompletion)
+        XCTAssertNil(result.rates)
+       
+        let actualError = try XCTUnwrap(result.error as NSError?)
+        XCTAssertEqual(actualError.domain, expectedError.domain)
+        XCTAssertEqual(actualError.code, expectedError.code)
+    }
+    
+    // MARK: - Queue - Tests
+    func test_getRates_givenHTTPStatusError_dispatchesToResponseQueue() {
+        verifyGetRatesDispatchedToMain(statusCode: 500)
+    }
+    
+    func test_getRates_givenError_dispatchesToResponseQueue() {
+        // Given
+        let error = NSError(domain: "com.RatesPatchTests", code: 42)
+
+        // Then
+        verifyGetRatesDispatchedToMain(error: error)
+    }
+    
+    func test_getRates_givenGoodResponse_dispatchesToResponseQueue() throws {
+        // Given
+        let data = try Data.fromJSON(fileName: "ExchangeRates")
+        
+        // Then
+        verifyGetRatesDispatchedToMain(data: data)
+    }
+    
+    func test_getRates_givenInvalidResponse_dispatchesToResponseQueue() throws {
+        // Given
+        let data = try Data.fromJSON(fileName: "GET_ExchangeRates_MissingValuesResponse")
+        
+        // Then
+        verifyGetRatesDispatchedToMain(data: data)
+    }
+}
+
+class MockURLSession: URLSession {
+    var queue: DispatchQueue? = nil
+    
+    func givenDispatchQueue() {
+        queue = DispatchQueue(label: "com.RatesPatchTests.MockSession")
+    }
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return MockURLSessionDataTask(completionHandler: completionHandler, url: url, queue: queue)
+    }
+}
+
+class MockURLSessionDataTask: URLSessionDataTask {
+    var completionHandler: (Data?, URLResponse?, Error?) -> Void
+    var url: URL
+    
+    init(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void,
+         url: URL,
+         queue: DispatchQueue?) {
+        if let queue = queue {
+            self.completionHandler = { data, response, error in
+                queue.async {
+                    completionHandler(data, response, error)
+                }
+            }
+        } else {
+            self.completionHandler = completionHandler
+        }
+        self.url = url
+        super.init()
+    }
+    
+    var calledResume = false
+    override func resume() {
+        calledResume = true
     }
 }
