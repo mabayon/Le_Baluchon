2f00464d016b85ece04e86437642d75e7f413bd8
mabayon <mabayon@student.42.fr>
2020-07-02 17:47:40 +0200

    Cleanning

--- Le BaluchonTests/Test Types/Mocks/MockURLSession.swift
+++ Le BaluchonTests/Test Types/Mocks/MockURLSession.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+class MockURLSession: URLSession {
+    var queue: DispatchQueue? = nil
+    
+    func givenDispatchQueue() {
+        queue = DispatchQueue(label: "com.Le_BaluchonTests.MockSession")
+    }
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return MockURLSessionDataTask(completionHandler: completionHandler, url: url, queue: queue)
+    }
+}
